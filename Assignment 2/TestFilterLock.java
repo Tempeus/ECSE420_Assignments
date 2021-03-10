5f75452dbc4fa1687b1aa132fd876b38f75e88a6
EXID JUNGHWA <jasonlau13@gmail.com>
2021-03-10 17:29:40 -0500

    Added new testfilerlock

--- Assignment 2/TestFilterLock.java
+++ Assignment 2/TestFilterLock.java
@@ -1,30 +1,52 @@
-public class TestFilterLock {
-    private final static int NTHREADS = 2;
-    private final static int COUNT = 100;
-    private  static int PER_THREAD = COUNT / NTHREADS;
-
-}
-
-    public static class FilterLockThread implements Runnable{
-
-        public int me;
-        public static final int countToThis = 10000;
-        public static final int noOfExperiments = 3;
-        public static volatile int count = 0;
-        public static int threadCount = 8;
-        public static FilterLock lock = new FilterLock(threadCount);
-
-        public FilterLockProgram(int newMe) {
-            me = newMe;
-        }
-
-        @Override
-        public void run() {
-            int i = 0;
-            while (i < countToThis) {
-                lock.lock();
-                count = count + 1;
-                i = i + 1;
-                lock.unlock();
-            }
-    }
+//public class TestFilterLock {
+//    private final static int NTHREADS = 2;
+//    private final static int COUNT = 100;
+//    private  static int PER_THREAD = COUNT / NTHREADS;
+//
+//    public static void main(String args[]) {
+//        int wrong = 0;
+//        long startTime = System.nanoTime();
+//
+//        for (int i = 0; i < noOfExperiments; i++) {
+//            count = 0;
+//
+//            spawnThreads();
+//
+//            // Check to see that count is correct value
+//            if (count != threadCount*countToThis) {
+//                System.out.println("Wrong : " + count);
+//                wrong++;
+//            }
+//        }
+//
+//        long endTime = System.nanoTime();
+//        System.out.println("That took " + (endTime - startTime)/1000000 + " milliseconds");
+//        System.out.println("Mistakes:  " + wrong + "/" + noOfExperiments);
+//    }
+//
+//}
+//
+//    public static class FilterLockThread implements Runnable {
+//
+//        public int me;
+//        public static final int countToThis = 10000;
+//        public static final int noOfExperiments = 3;
+//        public static volatile int count = 0;
+//        public static int threadCount = 8;
+//        public static FilterLock lock = new FilterLock(threadCount);
+//
+//        public FilterLockThread(int newMe) {
+//            me = newMe;
+//        }
+//
+//        @Override
+//        public void run() {
+//            int i = 0;
+//            while (i < countToThis) {
+//                lock.lock();
+//                count = count + 1;
+//                i = i + 1;
+//                lock.unlock();
+//            }
+//        }
+//    }
