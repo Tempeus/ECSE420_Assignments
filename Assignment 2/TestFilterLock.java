87077be4b23e98373b5233239fbe7147f14c33f6
EXID JUNGHWA <jasonlau13@gmail.com>
2021-03-10 18:43:55 -0500

    Refactored code

--- Assignment 2/TestFilterLock.java
+++ Assignment 2/TestFilterLock.java
@@ -2,20 +2,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class TestFilterLock {
-    private final static int NTHREADS = 2;
+    private final static int NTHREADS = 8;
     private final static int COUNT = 100;
     private static int PER_THREAD = COUNT / NTHREADS;
 
-    public static int threadCount = 8;
-    public static FilterLock lock = new FilterLock(threadCount);
+    public static FilterLock lock = new FilterLock(NTHREADS);
 
     public static void main(String[] args){
 
-        Thread[] filterLockThreads = new Thread[threadCount];
+        Thread[] filterLockThreads = new Thread[NTHREADS];
 
         ExecutorService executor = Executors.newFixedThreadPool(NTHREADS);
 
-        for(int i= 0; i< threadCount; i++){
+        for(int i= 0; i< NTHREADS; i++){
             filterLockThreads[i] = new Thread(new FilterLockThread());
             executor.execute(filterLockThreads[i]);
         }
@@ -29,11 +28,10 @@ public class TestFilterLock {
         @Override
         public void run() {
 
-//            for( int i = 0; i < PER_THREAD; i++){
-            while (true) {
+            for( int i = 0; i < PER_THREAD; i++){
+//            while (true) {
                 lock.lock();
                 lock.unlock(); // Release the lock
-//            }
             }
 
         }
