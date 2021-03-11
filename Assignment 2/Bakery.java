a62e4b38866320401130bad362275943c0a71327
Tempeus <nekugm@gmail.com>
2021-03-11 16:43:07 -0500

    boop

--- Assignment 2/Bakery.java
+++ Assignment 2/Bakery.java
@@ -2,78 +2,97 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 public class Bakery implements Lock {
-    boolean flag[];
-    Label[] label;
+    volatile boolean flag[];
+    volatile Label[] label;
 
-    public Bakery (int numofThreads){
+    public Bakery(int numofThreads) {
         flag = new boolean[numofThreads];
         label = new Label[numofThreads];
-        for (int i = 0; i < numofThreads; i++){
+        for (int i = 0; i < numofThreads; i++) {
             flag[i] = false;
             label[i] = new Label();
         }
     }
 
     @Override
-    public void lock(){
-        int id = ThreadID.get();
+    public void lock() {
+        int id = ThreadID.get() - 1;
         flag[id] = true;
-        //find the max number in labels and add 1
-        label[id].num = findMaxLabel() + 1;
-        int smol = findIndexOfMinLabel();
+        int max = Label.max(label);
+        label[id] = new Label(max + 1);
         /* if their label is smaller than ours, then wait for them to finish */
-        while(flag[smol] && label[smol].num < label[id].num);
-        System.out.println("ThreadID: "+id+ " with label "+ label[id].num);
+        while (conflict(id)) ;
+        System.out.println("ThreadID: " + id + " with label " + label[id].counter);
+    }
+
+    private boolean conflict(int id) {
+        for (int i = 0; i < label.length; i++) {
+            if (i != id && flag[i] && label[id].compareTo(label[i]) > -1) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
-    public void lockInterruptibly() throws InterruptedException {}
+    public void lockInterruptibly() throws InterruptedException {
+    }
 
     @Override
-    public boolean tryLock() {return false;}
+    public boolean tryLock() {
+        return false;
+    }
 
     @Override
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException { return false; }
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
 
     @Override
-    public void unlock(){
+    public void unlock() {
         int id = ThreadID.get();
         flag[id] = false;
-        label[id].num = 0;
-        System.out.println("ThreadID: "+ id+ " has finished queueing");
+        System.out.println("ThreadID: " + id + " has finished queueing");
     }
 
     @Override
-    public Condition newCondition() { return null; }
-
-    private int findMaxLabel(){
-        int max = label[0].num;
-
-        for(int i = 0; i < label.length; i++){
-            if(label[i].num > max){
-                max = label[i].num;
-            }
-        }
-        return max;
-    }
-
-    private int findIndexOfMinLabel(){
-        int min = label[0].index;
-
-        for(int i = 0; i < label.length; i++){
-            if(label[i].index < min){
-                min = label[i].index;
-            }
-        }
-        return min;
+    public Condition newCondition() {
+        return null;
     }
 }
 
-class Label{
-    public int num;
-    public int index;
-    public void Label(){
-        num = 0;
-        index = 0;
+class Label implements Comparable<Label> {
+    int counter;
+    int id;
+
+    Label() {
+        counter = 0;
+        id = ThreadID.get();
+    }
+
+    Label(int c) {
+        counter = c;
+        id = ThreadID.get();
+    }
+
+    static int max(Label[] labels) {
+        int c = 0;
+        for (Label label : labels) {
+            c = Math.max(c, label.counter);
+        }
+        return c;
+    }
+
+    @Override
+    public int compareTo(Label o) {
+        if (this.counter < o.counter || (this.counter == o.counter && this.id < o.id)) {
+            return -1;
+        }
+        else if (this.counter > o.counter) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
 }
