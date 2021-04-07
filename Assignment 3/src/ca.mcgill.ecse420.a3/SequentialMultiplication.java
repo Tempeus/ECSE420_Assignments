a22a532c57a887185e13e4abd996e2c4d54a4034
EXID JUNGHWA <jasonlau13@gmail.com>
2021-04-06 21:47:41 -0400

    Modified sequential shit

--- Assignment 3/src/ca.mcgill.ecse420.a3/SequentialMultiplication.java
+++ Assignment 3/src/ca.mcgill.ecse420.a3/SequentialMultiplication.java
@@ -1,11 +1,15 @@
 package ca.mcgill.ecse420.a3;
 
 public class SequentialMultiplication {
-    public static double[] sequentialMultiply(double[][] matrix, double[] vector, int matrix_size) {
-        double[] result = new double[matrix_size];
-        for (int i = 0; i < matrix_size; i++){
+    public static double[] sequentialMultiply(double[][] matrix, double[] vector) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        if (vector.length != n) throw new RuntimeException("Illegal matrix dimensions.");
+
+        double[] result = new double[m];
+        for (int i = 0; i < m; i++){
             result[i] = 0;
-            for (int j = 0; j < matrix_size; j++) {
+            for (int j = 0; j < m; j++) {
                 result[i] += matrix[i][j] * vector[j];
             }
         }
--- Assignment 3/src/ca.mcgill.ecse420.a3/TestMatrixMultiplication.java
+++ Assignment 3/src/ca.mcgill.ecse420.a3/TestMatrixMultiplication.java
@@ -1,4 +1,31 @@
 package ca.mcgill.ecse420.a3;
 
 public class TestMatrixMultiplication {
+
+    public static void printVec(double[] vec){
+        for(int i = 0; i<vec.length; i++){
+            System.out.println(vec[i]);
+        }
+    }
+    public static void printMatix(double[][] m){
+        for(int i = 0; i< m.length; i++){
+            for(int j = 0; j < m[0].length; j++){
+                System.out.print(" "+m[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        double[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+        double[] vector = {5,5,5};
+
+        double[] res = SequentialMultiplication.sequentialMultiply(matrix, vector);
+        printMatix(matrix);
+        System.out.println();
+        printVec(res);
+
+    }
+
+
 }
